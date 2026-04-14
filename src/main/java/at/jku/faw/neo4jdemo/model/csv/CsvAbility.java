f16c7616a48a1510a311035ede7af7a538e35f07
Bahara Muradi <bahara.muradi@jku.at>
2026-04-14 16:12:42 +0200

    refactor: delete redundant repo

--- src/main/java/at/jku/faw/neo4jdemo/model/csv/CsvAbility.java
+++ src/main/java/at/jku/faw/neo4jdemo/model/csv/CsvAbility.java
@@ -1,12 +1,20 @@
 package at.jku.faw.neo4jdemo.model.csv;
 
 import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record CsvAbility(
-		@CsvBindByName(column = "id") Long id,
-		@CsvBindByName(column = "identifier") String identifier,
-		@CsvBindByName(column = "is_main_series") int isMainSeries,
-		@CsvBindByName(column = "name") String name,
-		@CsvBindByName(column = "short_effect") String shortEffect,
-		@CsvBindByName(column = "effect") String effect
-) {}
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CsvAbility {
+	@CsvBindByName(column = "id") private Long id;
+	@CsvBindByName(column = "identifier") private String identifier;
+	@CsvBindByName(column = "is_main_series") private int isMainSeries;
+	@CsvBindByName(column = "name") private String name;
+	@CsvBindByName(column = "short_effect") private String shortEffect;
+	@CsvBindByName(column = "effect") private String effect;
+}

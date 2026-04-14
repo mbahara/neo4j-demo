adfcba22e00693ac28705a6a8d075fe24518b07c
Bahara Muradi <bahara.muradi@jku.at>
2026-04-14 15:36:49 +0200

    refactor: change the type of id to Long

--- src/main/java/at/jku/faw/neo4jdemo/model/neo4j/FormPokeathlonStats.java
+++ src/main/java/at/jku/faw/neo4jdemo/model/neo4j/FormPokeathlonStats.java
@@ -17,7 +17,7 @@ import org.springframework.data.neo4j.core.schema.TargetNode;
 public class FormPokeathlonStats {
     @Id
     @GeneratedValue
-    public String id;
+    public Long id;
 
     private int minimumStat;
     private int baseStat;
